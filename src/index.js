47a0d2438155e735bf9305f865bdd204aa4630ec
ergashev7 <dior7354423@gmail.com>
2023-07-15 21:41:56 +0500

    React Intro

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+const myClass = "blue";
+let title = <h3 className={myClass}>Hi react</h3>;
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <h1>React Intro</h1>
+    {title}
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
